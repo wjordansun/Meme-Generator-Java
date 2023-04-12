af912cbf257e90847587535176e29555df7b8219
sam-schmitt <samuelgschmitt@gmail.com>
2023-04-12 16:13:27 -0400

    top text, bottom text UI & UX

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,124 +1,130 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+const images = [
+	{
+		id: 1,
+		src: "https://via.placeholder.com/200x200",
+	},
+	{
+		id: 2,
+		src: "https://via.placeholder.com/200x200",
+	},
+	{
+		id: 3,
+		src: "https://via.placeholder.com/200x200",
+	},
+	{
+		id: 4,
+		src: "https://via.placeholder.com/200x200",
+	},
+	{
+		id: 5,
+		src: "https://via.placeholder.com/200x200",
+	},
+];
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [selectedImage, setSelectedImage] = useState(null);
+	const [topText, setTopText] = useState("");
+	const [bottomText, setBottomText] = useState("");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const handleImageClick = (image) => {
+		setSelectedImage(image);
+	};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const handleModalClose = () => {
+		setSelectedImage(null);
+		setTopText("");
+		setBottomText("");
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const handleTopTextChange = (e) => {
+		setTopText(e.target.value);
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+	const handleBottomTextChange = (e) => {
+		setBottomText(e.target.value);
+	};
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className='container mx-auto px-4'>
+			<ul className='flex flex-wrap -mx-4'>
+				{images.map((image) => (
+					<li
+						key={image.id}
+						className='w-full md:w-1/2 lg:w-1/3 px-4 mb-8 cursor-pointer'
+						onClick={() => handleImageClick(image)}
+					>
+						<div
+							className='relative w-full h-0'
+							style={{ paddingBottom: "66.67%" }}
+						>
+							<Image src={image.src} layout='fill' objectFit='cover' />
+						</div>
+					</li>
+				))}
+			</ul>
+			{selectedImage && (
+				<div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+					<div className='bg-white rounded-lg px-8 py-6'>
+						<div
+							className='relative w-full h-0'
+							style={{ paddingBottom: "66.67%" }}
+						>
+							<Image
+								src={selectedImage.src}
+								layout='fill'
+								objectFit='contain'
+							/>
+							<div
+								className='absolute top-0 left-0 w-full h-1/2 flex items-center justify-center text-white font-bold text-2xl'
+								style={{ textShadow: "1px 1px #000" }}
+							>
+								{topText}
+							</div>
+							<div
+								className='absolute bottom-0 left-0 w-full h-1/2 flex items-center justify-center text-white font-bold text-2xl'
+								style={{ textShadow: "1px 1px #000" }}
+							>
+								{bottomText}
+							</div>
+						</div>
+
+						<form className='mt-4'>
+							<div className='mb-4'>
+								<label className='block text-gray-700 font-medium mb-2'>
+									Top Text
+								</label>
+								<input
+									type='text'
+									className='block w-full rounded border-gray-400 shadow-sm py-2 px-3 leading-tight focus:outline-none focus:border-blue-500 text-black'
+									value={topText}
+									onChange={handleTopTextChange}
+								/>
+							</div>
+							<div className='mb-4'>
+								<label className='block text-gray-700 font-medium mb-2'>
+									Bottom Text
+								</label>
+								<input
+									type='text'
+									className='block w-full rounded border-gray-400 shadow-sm py-2 px-3 leading-tight focus:outline-none focus:border-blue-500 text-black'
+									value={bottomText}
+									onChange={handleBottomTextChange}
+								/>
+							</div>
+							<button
+								type='button'
+								className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'
+								onClick={handleModalClose}
+							>
+								Close
+							</button>
+						</form>
+					</div>
+				</div>
+			)}
+		</main>
+	);
 }
